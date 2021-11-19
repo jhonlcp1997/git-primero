f97c1b60f224089326dabca0a82f7cc875e37f9f
the-user-name <test@email.com>
2021-11-18 23:42:50 -0500

    All changes before create pjs1.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
+
+/*
+1111111
+variables: var, let, constant(no se puede modificar)
+formas de sacarlo: alert, prompt, document
+para poder concatenar numero empieza por un ""+numero1+numero2  o ${} pero con ``;
+
+LO QUE TRABAJE CON LO DE ARRIBA
 numero1 = 6;
 numero2 = 5;
 
 res1 = numero1 % numero2
 res2 = numero1 & numero2
 
-document.write(res1+" "+res2);
+document.write(res1+" "+res2+" ");
 
-
-/*
-1111111
-variables: var, let, constant(no se puede modificar)
-formas de sacarlo: alert, prompt, document
+rase1 = "Jhon Cruz";
+rase2 = " Estoy caminando ";
+document.write(rase1+rase2);
+document.write(`Soy ${rase1}`+rase2);
+-----------
 
 */
