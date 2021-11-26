1d63d224f2ac6f71b3a76e7b879315db39d8fdd5
the-user-name <test@email.com>
2021-11-25 19:33:33 -0500

    New desk js

---
+++ js/pruebasjs/cofla1.js
@@ -0,0 +1,73 @@
+
+dineroCofla = prompt("¿Cuanto dinero tienes Cofla?");
+dineroRoberto = prompt("¿Cuanto dinero tienes Roberto?");
+dineroPedro = prompt("¿Cuanto dinero tienes Pedro?");
+
+if(dineroCofla >= 0.6 && dineroCofla <1){
+	alert("Comprate helado de agua")
+}
+
+else if(dineroCofla >= 1 && dineroCofla <1.6){
+	alert("Comprate helado de crema")
+}
+else if(dineroCofla >= 1.6 && dineroCofla <1.7){
+	alert("Comprate helado de Heladix")
+}
+else if(dineroCofla >= 1.7 && dineroCofla <1.8){
+	alert("Comprate helado de heladovich")
+}
+else if(dineroCofla >= 1.8 && dineroCofla <2.9){
+	alert("Comprate helado de helardo")
+}
+else if(dineroCofla >= 2.9 ){
+	alert("Comprate helado de pote 1/$ con confites")
+}
+else {
+	alert("Eres pobre como yo");
+}
+
+if(dineroPedro >= 0.6 && dineroPedro <1){
+	alert("Comprate helado de agua")
+}
+
+else if(dineroPedro >= 1 && dineroPedro <1.6){
+	alert("Comprate helado de crema")
+}
+else if(dineroPedro >= 1.6 && dineroPedro <1.7){
+	alert("Comprate helado de Heladix")
+}
+else if(dineroPedro >= 1.7 && dineroPedro <1.8){
+	alert("Comprate helado de heladovich")
+}
+else if(dineroPedro >= 1.8 && dineroPedro <2.9){
+	alert("Comprate helado de helardo")
+}
+else if(dineroPedro >= 2.9 ){
+	alert("Comprate helado de pote 1/$ con confites")
+}
+else {
+	alert("Eres pobre como yo");
+}
+
+if(dineroRoberto >= 0.6 && dineroRoberto <1){
+	alert("Comprate helado de agua")
+}
+
+else if(dineroRoberto >= 1 && dineroRoberto <1.6){
+	alert("Comprate helado de crema")
+}
+else if(dineroRoberto >= 1.6 && dineroRoberto <1.7){
+	alert("Comprate helado de Heladix")
+}
+else if(dineroRoberto >= 1.7 && dineroRoberto <1.8){
+	alert("Comprate helado de heladovich")
+}
+else if(dineroRoberto >= 1.8 && dineroRoberto <2.9){
+	alert("Comprate helado de helardo")
+}
+else if(dineroRoberto >= 2.9 ){
+	alert("Comprate helado de pote 1/$ con confites")
+}
+else {
+	alert("Eres pobre como yo");
+}
